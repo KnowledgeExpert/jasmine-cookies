09ed7cbf431c75099c9237ba2a839fc9f8904514
Elly <olena.o.kushnir@gmail.com>
2018-06-13 11:38:22 +0300

    added Apache License

---
+++ NOTICE.md
@@ -0,0 +1,11 @@
+Jasmine-cookies
+Copyright 2018 Knowledge Expert SA
+
+This product includes software developed at
+The Apache Software Foundation (http://www.apache.org/).
+
+
+Other software with Apache 2.0 license included in this project:
+
+* typescript (has no NOTICE file)
+* SheetJS js-xlsx (http://sheetjs.com) (has no NOTICE file)
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,3 +1,17 @@
+// Copyright 2018 Knowledge Expert SA
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 import * as xlsx from 'xlsx';
 import {CsvToDeepJson} from 'csv-to-deep-json';
 import {Types} from './types';
@@ -7,7 +21,6 @@ import DataSourceInfo = Types.DataSourceInfo;
 import TestDataSourceType = Types.TestDataSourceType;
 import XlsxDataSourceInfo = Types.XlsxDataSourceInfo;
 import CsvDataSourceInfo = Types.CsvDataSourceInfo;
-
 
 export namespace Utils {
 
